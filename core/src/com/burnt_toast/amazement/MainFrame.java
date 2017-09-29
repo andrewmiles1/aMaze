353701ad13c8b6c1e8c75399708e103ccb6062e8
Andrew-Miles <elephant62@gmail.com>
2017-09-28 21:36:51 -0600

    Getting stuff set up
    Decided to NOT HAVE A SPLASH
    everything will be loaded in the MainFrame constructor
    started most of the file stuff, like some stuff that imports.

--- core/src/com/burnt_toast/amazement/MainFrame.java
+++ core/src/com/burnt_toast/amazement/MainFrame.java
@@ -1,9 +1,8 @@
 package com.burnt_toast.amazement;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
@@ -12,10 +11,14 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class MainFrame extends Game {
-	SpriteBatch batch;//le batch yo
+	public static SpriteBatch batch;//le batch yo
+	
+	public static AssetManager assets = new AssetManager();//Asset Manager
+	
+	public static final int SCREEN_WIDTH = 0;//16:10 ratio
+	public static final int SCREEN_HEIGHT = 0;
 	
 	//SCREENS
-	SplashScreen splashScr;
 	MenuScreen menuScr;
 	PlayScreen playScr;
 	
@@ -25,8 +28,22 @@ public class MainFrame extends Game {
 	public FreeTypeFontGenerator generator;
 	public FreeTypeFontParameter parameter;//used for loading fonts
 	
+	//Textures
+	public Texture gameTexture;
+	
 	public MainFrame(){
-		splashScr = new SplashScreen(this);
+		
+		//load font stuff
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-Light.ttf"));
+		parameter = new FreeTypeFontParameter();
+		parameter.size = 12;//FIXME will probably need bigger font size
+		gameFont = generator.generateFont(parameter);//generate font manually
+		
+		//load textures
+		gameTexture = MainFrame.assets.get("textures/mainTileset.png", Texture.class);
+		
+		//load sounds
+		
 		menuScr = new MenuScreen(this);
 		playScr = new PlayScreen(this);
 	}
@@ -36,6 +53,8 @@ public class MainFrame extends Game {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
 	
 	
 }
---
+++ core/src/com/burnt_toast/amazement/ScreenFadeTool.java
@@ -0,0 +1,74 @@
+package com.burnt_toast.amazement;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ScreenFadeTool {
+	private static boolean fadeIn;
+	private static boolean fadeOut;
+	private static float fadeTimer;//transparency amount from 0 to 1
+	private static float fadeFactor = 2;
+	private static String fadeCode;
+	private static SpriteBatch tempBatch;
+	private static BitmapFont tempFont;
+	
+	public static boolean isFading() {
+		//if either is true, return true,
+		//otherwise return false.
+		return (fadeIn || fadeOut)? true:false;
+	}
+	/**
+	 * updates the alpha/color to the font or batch you pass to it
+	 * to effectively fade.
+	 * @param t whatever you want updated/faded
+	 * @return returns the 'fade code' you entered when you told it to fade,
+	 *  So you know what to do when done. Returns "NONE" if no fade code.
+	 */
+	public static <T> String updateFade(T t) {
+		if(fadeIn) {
+			if(fadeTimer <1) {
+				fadeTimer += fadeFactor * Gdx.graphics.getDeltaTime();
+			}
+			else if(fadeTimer >= 1) {
+				fadeTimer = 1;
+				fadeIn = false;
+				return fadeCode;
+			}
+		}
+		else if(fadeOut) {
+			if(fadeTimer > 0) {
+				fadeTimer -= fadeFactor * Gdx.graphics.getDeltaTime();
+			}
+			else if(fadeTimer < 0) {
+				fadeTimer = 0;
+				fadeOut = false;
+				return fadeCode;
+			}
+		}
+		if(t.getClass() == SpriteBatch.class) {
+			//if sprite batch
+			tempBatch = (SpriteBatch)t;
+			tempBatch.setColor(tempBatch.getColor().r, 
+					tempBatch.getColor().g, tempBatch.getColor().b, fadeFactor);
+		}
+		if(t.getClass() == BitmapFont.class) {
+			//if bitmap font
+			tempFont = (BitmapFont)t;
+			tempFont.setColor(tempFont.getColor().r,
+					tempFont.getColor().g, tempFont.getColor().b, fadeFactor);
+		}
+		return "NONE";
+	}
+	public static void fadeIn(String passFadeCode) {
+		fadeCode = passFadeCode;
+		if(fadeOut)fadeOut = false;
+		fadeIn = true;//make sure that it's set to fade in
+	}
+	public static void fadeOut(String passFadeCode) {
+		fadeCode = passFadeCode;
+		if(fadeOut)fadeOut = false;
+		fadeOut = true;//make sure that it's set to fade out
+	}
+
+}
